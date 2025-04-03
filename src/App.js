2c8344ca1f5fd065ac581cb0f01cced863d1a5bb
Pierce Irvin <pirvin@tcetra.local>
2025-04-03 14:44:56 -0400

    added stuff like websockets and a submit button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import RestaurantItem from './components/restaurantItem';
 import { useState, useEffect } from 'react';
 
 function App() {
+  let socket = new WebSocket('ws://10.16.118.201:3001/');
+
   const [loading, setLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
 
@@ -22,13 +24,42 @@ function App() {
     fetchAvailableRestaurants();
   }, []);
 
-  const columnSectionStyle = {
+  useEffect(() => {
+    socket.onopen = () => {
+      alert("Let's vote!");
+    };
+
+    // socket.onmessage = (event) => {
+    //   console.log(event.data);
+    // };
+  }, []);
+
+  const choicesSectionStyle = {
     width: '40vw',
     height: '100vh',
     display: 'inline-block',
   }
 
-  const handleClick = (rest) => {
+  const restHeaderstyle = { 
+    // width: '100%',
+    // flexDirection: 'row'
+  }
+
+  const submitButtonStyle = {
+    height: 48,
+    width: 140,
+    backgroundColor: '#ff6060',
+    color: '#fff',
+    fontWeight: 'bold',
+    borderRadius: 24,
+    borderWidth: 0.5,
+    fontSize: 20,
+    position: 'absolute',
+    right: 24,
+    top: 48,
+  }
+
+  const handleRestaurantClick = (rest) => {
     if (selectedRestaurants.length === 3 && !selectedRestaurants.includes(rest)) {
       alert("Hey stop. 3 votes max >:(");
       return;
@@ -53,17 +84,22 @@ function App() {
         <div style={{ textAlign: 'left', width: '100%', padding: 12 }}>
           <h1>
             Lunchatron 3000 <span style={{ fontSize: 16 }}>by: <a style={{ color: '#fff', textDecorationLine: 'none'}} href="twitch.tv/kattzz">kattzz</a></span>
+            <button style={submitButtonStyle}>
+              Submit
+            </button>
           </h1>
         </div>
       </section>
       <section>
-        <div style={columnSectionStyle}>
-          <h2>Restaurants</h2>
+        <div style={choicesSectionStyle}>
+          <div style={restHeaderstyle}>
+            <h2>Restaurants</h2>
+          </div>
           { availableRestaurants.map((rest, index) => {
             return(
               <RestaurantItem
                 key={index}
-                onClick={() => handleClick(rest)}
+                onClick={() => handleRestaurantClick(rest)}
                 restaurant={rest}
                 selectedItems={selectedRestaurants}
                 disableItem={selectedRestaurants.length === 3 && !selectedRestaurants.includes(rest)}
