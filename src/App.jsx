d06b57dad78ef339bb5106e09400729059049491
Pierce Irvin <pirvin@tcetra.local>
2025-04-23 09:02:26 -0400

    moved stuff around, added a start timer button

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import RestaurantItem from './components/restaurantItem';
 import { useState, useEffect } from 'react';
+import { jsx } from 'react/jsx-runtime';
 
 function App() {
   let socket = new WebSocket('ws://10.16.118.201:3001/');
 
   const [loading, setLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
+  const [showVotingTimer, setShowVotingTimer] = useState(false);
 
   const [availableRestaurants, setAvailableRestaurants] = useState([]);
   const [selectedRestaurants, setSelectedRestaurants] = useState([]);
@@ -26,7 +28,7 @@ function App() {
 
   useEffect(() => {
     socket.onopen = () => {
-      alert("Let's vote!");
+      // setShowVotingTimer(true);
     };
 
     // socket.onmessage = (event) => {
@@ -34,15 +36,40 @@ function App() {
     // };
   }, []);
 
+  const adminSectionStyle = {
+    display: 'flex',
+    width: '40vw',
+    height: '70vh',
+    // display: 'inline-block',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
   const choicesSectionStyle = {
     width: '40vw',
     height: '100vh',
     display: 'inline-block',
   }
 
-  const restHeaderstyle = { 
-    // width: '100%',
-    // flexDirection: 'row'
+  const headerstyle = {
+    width: '90%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 24,
+    paddingTop: 24,
+    paddingBottom: 8,
+  }
+
+  const startTimerButtonStyle = {
+    height: 72,
+    width: 240,
+    backgroundColor: '#ff6060',
+    color: '#fff',
+    fontWeight: 'bold',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    fontSize: 32,
   }
 
   const submitButtonStyle = {
@@ -54,9 +81,12 @@ function App() {
     borderRadius: 24,
     borderWidth: 0.5,
     fontSize: 20,
-    position: 'absolute',
-    right: 24,
-    top: 48,
+  }
+
+  const timerLabelStyle = { 
+    fontWeight: 'bold', 
+    fontSize: 32, 
+    color: 'green',
   }
 
   const handleRestaurantClick = (rest) => {
@@ -78,36 +108,52 @@ function App() {
     });
   }
 
+  const handleSubmitClick = async () => {
+    alert("Submit: " + selectedRestaurants + '?');
+  }
+
   return (
     <div className="App">
       <section className="App-header">
         <div style={{ textAlign: 'left', width: '100%', padding: 12 }}>
           <h1>
             Lunchatron 3000 <span style={{ fontSize: 16 }}>by: <a style={{ color: '#fff', textDecorationLine: 'none'}} href="twitch.tv/kattzz">kattzz</a></span>
-            <button style={submitButtonStyle}>
-              Submit
-            </button>
           </h1>
         </div>
       </section>
-      <section>
-        <div style={choicesSectionStyle}>
-          <div style={restHeaderstyle}>
-            <h2>Restaurants</h2>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <section>
+          <div style={adminSectionStyle}>
+            <button onClick={handleSubmitClick} style={startTimerButtonStyle}>
+              Start Timer
+            </button>
           </div>
-          { availableRestaurants.map((rest, index) => {
-            return(
-              <RestaurantItem
-                key={index}
-                onClick={() => handleRestaurantClick(rest)}
-                restaurant={rest}
-                selectedItems={selectedRestaurants}
-                disableItem={selectedRestaurants.length === 3 && !selectedRestaurants.includes(rest)}
-              />
-            );
-          })}
-        </div>
-      </section>
+        </section>
+        <section>
+          <div style={choicesSectionStyle}>
+            <div style={headerstyle}>
+              <span style={{ fontWeight: 'bold', fontSize: 32 }}>Restaurants</span>
+              <button onClick={handleSubmitClick} style={submitButtonStyle}>
+                Submit
+              </button>
+            </div>
+            { availableRestaurants.map((rest, index) => {
+              return(
+                <RestaurantItem
+                  key={index}
+                  onClick={() => handleRestaurantClick(rest)}
+                  restaurant={rest}
+                  selectedItems={selectedRestaurants}
+                  disableItem={selectedRestaurants.length === 3 && !selectedRestaurants.includes(rest)}
+                />
+              );
+            })}
+          </div>
+          { showVotingTimer &&
+            <span style={timerLabelStyle}>Voting is LIVE</span>
+          }
+        </section>
+      </div>
     </div>
   );
 }
